a8e67761b2177a5b436f590332489175da6217cb
Alex Fioto <afioto@icloud.com>
2020-10-27 16:39:41 -0400

    ;adfasd

---
+++ app_starter.py
@@ -0,0 +1,27 @@
+# imports
+from flask import Flask, Response, request, render_template
+from chatterbot import ChatBot
+
+
+app = Flask('Seinfeld Chatbot')
+
+bot = ChatBot('Flask_app',
+               database_uri='sqlite:///db.sqlite3')
+
+
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/get')
+def get_bot_response():
+    user_text = request.args.get('msg')
+    return str(bot.get_response(user_text))
+
+
+
+# Call app.run(debug=True) when python script is called
+if __name__ == '__main__':
+    app.run(debug=True)
