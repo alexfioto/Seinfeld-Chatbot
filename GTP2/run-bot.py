bc15a09be6b6cd2f654974a192c84dd248a7491c
Alex Fioto <afioto@icloud.com>
2020-11-15 13:44:02 -0500

    app edits

--- GTP2/run-bot.py
+++ GTP2/run-bot.py
@@ -218,7 +218,7 @@ class SeinfeldChatbot():
                         print(plot)
                     res = input('Do you want to watch the show?\n>>> ')
                     if res.lower() in self.positive_responses:
-                        webbrowser.open_new(f'https://youtube.com/results?search_query=seinfeld+season+{self.scores_list_[i][0]}+episode+{self.scores_list_[i][1]}')
+                        webbrowser.open_new(f'https://youtube.com/results?search_query=seinfeld+season+{episode}')
                     res = input('JERRY: Do you want another recommendation?\n>>> ')
                     if res == 'no':
                         print('JERRY: OK.')
--- GTP2/seinfeldbot.py
+++ GTP2/seinfeldbot.py
@@ -179,7 +179,7 @@ class SeinfeldChatbot():
                     print(plot)
                 res = input('Do you want to watch the show?')
                 if res.lower() in self.positive_responses:
-                    webbrowser.open_new(f'https://youtube.com/results?search_query=seinfeld+season+{self.scores_list_[i][0]}+episode+{self.scores_list_[i][1]}')
+                    webbrowser.open_new(f'https://youtube.com/results?search_query=seinfeld+season+{episode}')
                 res = input('JERRY: Do you want another recommendation?')
                 if res == 'no':
                     print('JERRY: OK.')
